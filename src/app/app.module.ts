2d285b7ac089bf66c5ff81220afe85b44d784c7e
Armin Kovacs <armin0191@gmail.com>
2020-07-13 15:27:27 +0200

    Remove placeholder html and add compononents and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { JokeComponent } from './joke/joke.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    JokeComponent
   ],
   imports: [
     BrowserModule,
