43e2427b7f0e58a8995a227a890bff4247cbc242
Armin Kovacs <armin0191@gmail.com>
2020-07-14 10:04:52 +0200

    Add image to welcome page, tweak css of welcome page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, JokeComponent],
@@ -21,6 +21,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatTabsModule,
     MatCardModule,
     MatButtonModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent],
